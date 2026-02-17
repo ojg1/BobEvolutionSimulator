ad5b64cd71a0abf9b5be1d6c9dbaf73ccd89273e
OrangeJuiceGuy1 <ericlin1253@gmail.com>
2026-02-16 20:25:21 -0500

    backpropagate functionality

--- utilsBob.py
+++ utilsBob.py
@@ -13,25 +13,67 @@ def RectifiedLinearUnit(z):
         d = 1
     return (max(0, z), d)
 
-def Backpropagate(oNeuronIndex : int, outerLayer : list, firstLayer : list, loss : int, TDerror : int):
+
+def ForwardPass(firstLayer : list, outerLayer : list, dx : int, dy :int):
+    '''
+    func ForwardPass()
+    
+    :type firstLayer: list{weights:[... : int](2), bias, result}
+    :type outerLayer: list{weights:[... : int](4), bias, result}
+    :type dx: int
+    :type dy: int
+
+    Specifically designed for 2-4-4
+
+    '''
+
+
+
+def Backpropagate(oNeuronIndex : int, outerLayer : list, firstLayer : list, TDerror:int, loss:int, Alpha : int, xinp : int, yinp: int):
     
     '''
     func Backpropagate()
     :type oNeuronIndex: int
     :type outerLayer: list{weights:[... : int](4), bias, result}
     :type firstLayer: list{weights:[... : int](2), bias, result}
-    :type loss: int
-    :type TDerror: int
-    
+    :type TDerror: int, error
+    :type loss: int, TDerror^2
+    :type Alpha: int, learning rate
+
     Specifically designed for 2-4-4
 
     
 
     '''
 
-    outputNeuron = outerLayer[oNeuronIndex]
-    outputNeuronBias    = outputNeuron[2] 
-    outputNeuronWeights = outputNeuron[0] #hidden
+    outerLayer[oNeuronIndex][1] += Alpha * TDerror #change bias
+    outputNeuronWeights = outerLayer[oNeuronIndex][0].copy() #hidden -> output weights
+
+    #adjust h -> o weights
+
+    for iWeight in range(len(outputNeuronWeights)):
+        #responsible neuron    corr. weight    lr      error     res of corr. neuron
+        outerLayer[oNeuronIndex][0][iWeight] += Alpha * TDerror * RectifiedLinearUnit(firstLayer[iWeight][2])[0]
+
+    #adjust in -> h weights
+
+    for iWeight in range(len(firstLayer)):
+        blameDegree = TDerror * outputNeuronWeights[iWeight]
+        blameDegree = blameDegree * RectifiedLinearUnit(firstLayer[iWeight][2])[1]
+        blameDegreeX = Alpha * blameDegree * xinp
+        blameDegreeY = Alpha * blameDegree * yinp
+        firstLayer[iWeight][0][0] += blameDegreeX
+        firstLayer[iWeight][0][1] += blameDegreeY
+
+        #bias change
+        firstLayer[iWeight][1] = firstLayer[iWeight][1] + (Alpha * blameDegree)
+
+
+
+    return [firstLayer, outerLayer]
+
+
+
     
 
 
